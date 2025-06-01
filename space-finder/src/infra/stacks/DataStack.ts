1cf2690b1fb461b4da850461371a8e5b3c1e07bb
Onur Degerli <onur.degerli@viviotex.com>
2025-06-01 13:07:22 +0200

    Integrate LabdaStack and ApiGatewayStack and link to each other.

--- space-finder/src/infra/stacks/DataStack.ts
+++ space-finder/src/infra/stacks/DataStack.ts
@@ -1,8 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
+import { AttributeType, ITable, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
+import { getSuffixFromStack } from '../Utils';
 
 export class DataStack extends cdk.Stack {
+
+  public readonly spacesTable: ITable;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const suffix = getSuffixFromStack(this);
+
+    this.spacesTable = new Table(this, 'SpacesTable', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING,
+      },
+      tableName: `SpaceStack-${suffix}`
+    });
   }
 }
---
+++ space-finder/src/infra/stacks/LambdaStack.ts
@@ -0,0 +1,30 @@
+import * as cdk from 'aws-cdk-lib';
+import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
+import { ITable } from 'aws-cdk-lib/aws-dynamodb';
+import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+import { join } from 'path';
+
+interface LambdaStackProps extends cdk.StackProps {
+  spacesTable: ITable,
+}
+
+export class LambdaStack extends cdk.Stack {
+
+  public readonly helloLambdaIntegration: LambdaIntegration;
+
+  constructor(scope: Construct, id: string, props?: LambdaStackProps) {
+    super(scope, id, props);
+
+    const helloLambda = new LambdaFunction(this, 'HelloLambda', {
+      runtime: Runtime.NODEJS_LATEST,
+      handler: 'hello.main',
+      code: Code.fromAsset(join(__dirname, '..', '..', 'services')),
+      environment: {
+        TABLE_NAME: props.spacesTable.tableName
+      }
+    })
+
+    this.helloLambdaIntegration = new LambdaIntegration(helloLambda);
+  }
+}
