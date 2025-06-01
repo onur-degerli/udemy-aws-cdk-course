fed393f03f01b7e0acceb14014dedff3c805fe07
Onur Degerli <onur.degerli@viviotex.com>
2025-06-01 14:08:44 +0200

    Use sdk functions. List all s3 buckets in lambda

--- space-finder/src/services/hello.ts
+++ space-finder/src/services/hello.ts
@@ -1,10 +1,18 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
 import { v4 } from 'uuid'
+import { S3Client, ListBucketsCommand } from '@aws-sdk/client-s3';
+
+const s3Client = new S3Client({});
 
 async function handler(event: APIGatewayProxyEvent, context: Context) {
+
+  const command = new ListBucketsCommand({});
+  const listBucketsResult = (await s3Client.send(command)).Buckets;
+
   const response: APIGatewayProxyResult = {
     statusCode: 200,
-    body: JSON.stringify('Hello from lambda! This is the id: ' + v4())
+    body: JSON.stringify('Hello from lambda! This is the id: ' + v4() 
+      + ' Here are the buckets: ' + JSON.stringify(listBucketsResult))
   }
 
   console.log(event);
