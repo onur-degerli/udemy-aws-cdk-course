185a00739b465deb0e669eff36c6a22fb7b15866
Onur Degerli <onur.degerli@viviotex.com>
2025-06-02 21:47:14 +0200

    Implement new ApiGateway endpoints

--- space-finder/src/infra/stacks/LambdaStack.ts
+++ space-finder/src/infra/stacks/LambdaStack.ts
@@ -1,8 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 import { ITable } from 'aws-cdk-lib/aws-dynamodb';
-import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
-import { Code, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 import { join } from 'path';
@@ -13,29 +12,20 @@ interface LambdaStackProps extends cdk.StackProps {
 
 export class LambdaStack extends cdk.Stack {
 
-  public readonly helloLambdaIntegration: LambdaIntegration;
+  public readonly spacesLambdaIntegration: LambdaIntegration;
 
   constructor(scope: Construct, id: string, props?: LambdaStackProps) {
     super(scope, id, props);
 
-    const helloLambda = new NodejsFunction(this, 'HelloLambda', {
+    const spacesLambda = new NodejsFunction(this, 'SpacesLambda', {
       runtime: Runtime.NODEJS_LATEST,
       handler: 'handler',
-      entry: join(__dirname, '..', '..', 'services', 'hello.ts'),
+      entry: join(__dirname, '..', '..', 'services', 'spaces', 'handler.ts'),
       environment: {
         TABLE_NAME: props.spacesTable.tableName
       }
     });
 
-    helloLambda.addToRolePolicy(new PolicyStatement({
-      effect: Effect.ALLOW,
-      actions: [
-        's3:ListAllMyBuckets',
-        's3:ListBucket'
-      ],
-      resources: ["*"] // bad practice
-    }))
-
-    this.helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    this.spacesLambdaIntegration = new LambdaIntegration(spacesLambda);
   }
 }
