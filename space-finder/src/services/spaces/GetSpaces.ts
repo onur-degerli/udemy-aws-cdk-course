df82efa83f824de50b4c62ab0ae9cd4bb257314b
Onur Degerli <onur.degerli@viviotex.com>
2025-06-14 23:01:34 +0200

    Use marshall and unmarshall methods

--- space-finder/src/services/spaces/GetSpaces.ts
+++ space-finder/src/services/spaces/GetSpaces.ts
@@ -1,16 +1,52 @@
-import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient, GetItemCommand, ScanCommand } from "@aws-sdk/client-dynamodb";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
 export async function getSpaces(event: APIGatewayProxyEvent, ddbClient: DynamoDBClient): Promise<APIGatewayProxyResult> {
 
-  const result = await ddbClient.send(new ScanCommand({
-    TableName: process.env.TABLE_NAME,
-  }));
+  if (event.queryStringParameters) {
+    if ('id' in event.queryStringParameters) {
+      const spaceId = event.queryStringParameters['id'];
+      const getItemResponse = await ddbClient.send(new GetItemCommand({
+        TableName: process.env.TABLE_NAME,
+        Key: {
+          'id': {
+            S: spaceId
+          }
+        }
+      }));
 
-  console.log(result.Items);
+      if (getItemResponse.Item) {
+        const unmarshalledItem = unmarshall(getItemResponse.Item);
+        console.log(unmarshalledItem);
+        return {
+          statusCode: 200,
+          body: JSON.stringify(unmarshalledItem)
+        }
+      } else {
+        return {
+          statusCode: 404,
+          body: JSON.stringify(`Space with id ${spaceId} not found`)
+        }
+      }
+    } else {
+      return {
+        statusCode: 401,
+        body: JSON.stringify('Id required!')
+      }
+    }
+  } else {
+    const result = await ddbClient.send(new ScanCommand({
+      TableName: process.env.TABLE_NAME,
+    }));
 
-  return {
-    statusCode: 201,
-    body: JSON.stringify(result.Items)
+    const unmarshalledItems = result.Items?.map(item => unmarshall(item));
+
+    console.log(unmarshalledItems);
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify(unmarshalledItems)
+    }
   }
 }
--- space-finder/test/launcher.ts
+++ space-finder/test/launcher.ts
@@ -5,6 +5,9 @@ process.env.TABLE_NAME = 'SpaceStack-0a39509a9a59';
 
 handler({
   httpMethod: 'GET',
+  queryStringParameters: {
+    id: '8d3dee85-5a19-4a68-9eeb-9e8fbbecfac1'
+  }
   /* body: JSON.stringify({
     location: 'Dublin'
   }) */
