34236c473843c12ed609657376c9aa72d27c7a3a
Onur Degerli <onur.degerli@viviotex.com>
2025-06-24 22:59:20 +0200

    Implement UiDeployment stack

--- space-finder/src/infra/Launcher.ts
+++ space-finder/src/infra/Launcher.ts
@@ -3,6 +3,7 @@ import { DataStack } from "./stacks/DataStack";
 import { LambdaStack } from "./stacks/LambdaStack";
 import { ApiStack } from "./stacks/ApiStack";
 import { AuthStack } from "./stacks/AuthStack";
+import { UiDeploymentStack } from "./stacks/UiDeploymentStack";
 
 const app = new App();
 const dataStack = new DataStack(app, 'DataStack');
@@ -15,4 +16,6 @@ const authStack = new AuthStack(app, 'AuthStack');
 new ApiStack(app, 'ApiStack', {
   spacesLambdaIntegration: lambdaStack.spacesLambdaIntegration,
   userPool: authStack.userPool
-})
+});
+
+new UiDeploymentStack(app, 'UiDeploymentStack');
---
+++ space-finder/src/infra/stacks/UiDeploymentStack.ts
@@ -0,0 +1,48 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { getSuffixFromStack } from '../Utils';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { join } from 'path';
+import { existsSync } from 'fs';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { AccessLevel, Distribution } from 'aws-cdk-lib/aws-cloudfront';
+import { S3BucketOrigin } from 'aws-cdk-lib/aws-cloudfront-origins';
+
+export class UiDeploymentStack extends cdk.Stack {
+
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const suffix = getSuffixFromStack(this);
+
+    const deploymentBucket = new Bucket(this, 'uiDeploymentBucket', {
+      bucketName: `space-finder-frontend-${suffix}`
+    });
+
+    const uiDir = join(__dirname, '..', '..', '..', '..', 'space-finder-frontend', 'dist');
+    if (!existsSync(uiDir)) {
+      console.warn('UI directory not found: ' + uiDir);
+    }
+
+    new BucketDeployment(this, 'SpacesFinderDeployment', {
+      destinationBucket: deploymentBucket,
+      sources: [Source.asset(uiDir)]
+    });
+
+    const s3Origin = S3BucketOrigin.withOriginAccessControl(deploymentBucket, {
+        originAccessLevels: [AccessLevel.READ],
+    });
+
+    const distribution = new Distribution(this, 'SpacesFinderDistribution', {
+        defaultRootObject: 'index.html',
+        defaultBehavior: {
+            origin: s3Origin
+        }
+    });
+
+    new cdk.CfnOutput(this, 'SpaceFInderUrl', {
+        value: distribution.distributionDomainName
+    })
+  }
+}
