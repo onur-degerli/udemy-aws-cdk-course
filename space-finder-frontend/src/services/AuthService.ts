5d215640b3b4914a2a17f1b405e955da6bfef42f
Onur Degerli <onur.degerli@bim.com>
2025-07-06 22:53:30 +0200

    Completing UI.

--- space-finder-frontend/src/services/AuthService.ts
+++ space-finder-frontend/src/services/AuthService.ts
@@ -1,27 +1,27 @@
-import { type CognitoUser } from '@aws-amplify/auth';
-import { Amplify, Auth } from 'aws-amplify';
+import { SignInOutput, fetchAuthSession, signIn, getCurrentUser, AuthUser } from '@aws-amplify/auth';
+import { Amplify } from 'aws-amplify';
 import { AuthStack } from '../../../space-finder/outputs.json';
 import { CognitoIdentityClient } from '@aws-sdk/client-cognito-identity';
 import { fromCognitoIdentityPool } from '@aws-sdk/credential-providers';
 
-const awsRegion = 'eu-west-1';
+const awsRegion = 'eu-north-1';
 
 Amplify.configure({
     Auth: {
-        mandatorySignIn: false,
-        region: awsRegion,
+      Cognito: {
         userPoolId: AuthStack.SpaceUserPoolId,
-        userPoolWebClientId: AuthStack.SpaceUserPoolClientId,
-        identityPoolId: AuthStack.SpaceIdentityPoolId,
-        authenticationFlowType: 'USER_PASSWORD_AUTH'
-    }
-})
+        userPoolClientId: AuthStack.SpaceUserPoolClientId,
+        identityPoolId: AuthStack.SpaceIdentityPoolId,        
+      },
+    },
+  });
 
 export class AuthService {
 
-    private user: CognitoUser | undefined;
+    private user: SignInOutput | AuthUser |undefined;
     public jwtToken: string | undefined;
     private temporaryCredentials: object | undefined;
+    private userName: string ='';
 
     public isAuthorized(){
         if (this.user) {
@@ -30,15 +30,49 @@ export class AuthService {
         return false;
     }
 
+    /**
+     * call only after login
+     */
+    public async getIdToken(){
+        const authSession = await fetchAuthSession();
+        console.log('authSession', authSession)
+        return authSession.tokens?.idToken?.toString();
+    }
 
     public async login(userName: string, password: string): Promise<Object | undefined> {
         try {
-            this.user = await Auth.signIn(userName, password) as CognitoUser;
-            this.jwtToken = this.user?.getSignInUserSession()?.getIdToken().getJwtToken();
+            // check if user is already logged in
+            const user = await this.getCurrentUser();
+            if (user) {
+                this.user = user;
+            } else { 
+                const signInOutput: SignInOutput = await signIn({
+                    username: userName,
+                    password: password,
+                    options: {
+                        authFlowType: 'USER_PASSWORD_AUTH'
+                    }
+                });
+                this.user = signInOutput;
+            }
+
+            this.userName = userName;
+            this.jwtToken = await this.getIdToken();
+
+            console.log('jwt token', this.jwtToken)
             return this.user;
         } catch (error) {
             console.error(error);
             return undefined
+        }
+    }
+
+    private async getCurrentUser(){
+        try {
+            const user = await getCurrentUser();
+            return user;
+        } catch (error) {
+            return undefined;
         }
     }
 
@@ -50,8 +84,8 @@ export class AuthService {
         return this.temporaryCredentials;
     }
 
-    public getUserName() {
-        return this.user?.getUsername();
+    public getUserName(){
+        return this.userName;
     }
 
     private async generateTemporaryCredentials() {
