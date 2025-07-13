235915a1ac7c360ef6fea715a28c8570af2146ec
Onur Degerli <onur.degerli@bim.com>
2025-07-13 15:13:39 +0200

    Initial pipeline.

--- cdk-cicd/lib/cdk-cicd-stack.ts
+++ cdk-cicd/lib/cdk-cicd-stack.ts
@@ -1,8 +1,25 @@
-import * as cdk from "aws-cdk-lib";
-import { Construct } from "constructs";
+import * as cdk from 'aws-cdk-lib';
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from 'aws-cdk-lib/pipelines';
+import { Construct } from 'constructs';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new CodePipeline(this, 'AwasomePipeline', {
+      pipelineName: 'AwasomePipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub(
+          'onur-degerli/udemy-aws-cdk-course',
+          'cicd-practice'
+        ),
+        commands: ['cd cdk-cicd', 'npm ci', 'npx cdk synth'],
+        primaryOutputDirectory: 'cdk-cicd/cdk.out',
+      }),
+    });
   }
 }
